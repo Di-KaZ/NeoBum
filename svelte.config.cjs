0ac24107faf42e84467edc7ef2f2cbe8bb0030a2
GET MOUSSED <mofanafoussa@gmail.com>
2021-05-02 17:59:43 +0200

    trying heroku

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,13 +1,12 @@
 const preprocess = require('svelte-preprocess');
-const vercel = require('@sveltejs/adapter-vercel');
+const node = require('@sveltejs/adapter-node');
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   preprocess: preprocess(),
-
   kit: {
-    adapter: vercel(),
+    adapter: node(),
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
     vite: {
