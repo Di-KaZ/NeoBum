212518411184c577c859e465758df2d940d0bf88
GET MOUSSED <mofanafoussa@gmail.com>
2021-05-02 18:16:47 +0200

    added procfile

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,12 +1,10 @@
 const preprocess = require('svelte-preprocess');
-const node = require('@sveltejs/adapter-node');
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   preprocess: preprocess(),
   kit: {
-    adapter: node(),
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
     vite: {
